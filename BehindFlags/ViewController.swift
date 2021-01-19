bbedf45cc3de958922ffab04287419bcf30aa6b7
Fernando Lobo <fernandosclb@gmail.com>
2021-01-19 11:16:34 -0300

    First commit

---
+++ BehindFlags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BehindFlags
+//
+//  Created by Fernando Lobo on 19/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
